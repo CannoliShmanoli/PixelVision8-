69457f28b7b3e2315ff0be8057ae7cc4e490ed5a
HolyCannoli <annalynnfan@gmail.com>
2026-01-31 13:20:24 -0500

    Update GameChip.6502.cs

--- SDK/Player/Chips/Game/GameChip.6502.cs
+++ SDK/Player/Chips/Game/GameChip.6502.cs
@@ -1,1 +1,12 @@
-
+namespace PixelVision8.Assembly
+{
+    /// <summary>
+    ///     The 6502 Chip is an implementation of 6502 assembly,
+    ///     with all the logic it needs to work correctly in the PixelVisionEngine.
+    ///     It's an easy way to use Assembly in PV8+ with minor caveats.
+    /// </summary>
+    public partial class GameChip
+    {
+        public bool lockSpecs = false;
+      /// nothing here yet...
+}
